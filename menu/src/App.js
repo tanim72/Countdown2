b95df0f076e17ed7945ec6a4964e6bcc9b35f1e6
Tanisha Mehta <tanishamehta@tanishas-mbp.wifi.local.cmu.edu>
2023-04-01 13:58:42 -0400

    menu project - wifi cutout, not much progress

---
+++ menu/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const data = {
+    "breakfast": [
+      {"food": "pancakes", "price": 5.00, "vegetarian": true},
+      {"food": "waffles", "price": 5.00, "vegetarian": true},
+      {"food": "orange juice", "price": 2.00, "vegetarian": true}
+    ],
+    "lunch": [
+      {"food": "turkey sandwich", "price": 8.00, "vegetarian": false},
+      {"food": "grilled cheese", "price": 6.00, "vegetarian": true},
+      {"food": "hamburger", "price": 8.00, "vegetarian": false}
+    ],
+    "dinner": [
+      {"food": "chicken alfredo", "price": 10.00, "vegetarian": false},
+      {"food": "tofu stir-fry", "price": 9.00, "vegetarian": true},
+      {"food": "chili", "price": 8.00, "vegetarian": false}
+    ]
+  }
+  
+
+  return (
+    <div className="App">
+      <Menu menu = {data} />
+    </div>
+  );
+}
+
+export default App;
---
+++ menu/src/Menu.js
@@ -0,0 +1,5 @@
+
+function Menu(props) {
+    const[ showOnlyVegetarian, setShowOnlyVegetarian] = useState(false);
+    
+}
